56155dfd2298119d92772eb7a53fb24ee10b1afd
Luka Djigas <ldigas@gmail.com>
2011-04-20 03:54:57 +0200

    A new line on the "new" branch.

--- HelloWorld.f90
+++ HelloWorld.f90
@@ -3,6 +3,7 @@ write(*,'("A new line for a 2nd commit!")')
 write(*,'("A new line for the 3nd commit!")')
 
 !Comment from the newly founded "new" branch
+!Comment from the newly founded "new" branch
 
 end
 
--- HelloWorld.f90~
+++ HelloWorld.f90~
@@ -1,6 +1,9 @@
 write(*,'("Hello World!")')
 write(*,'("A new line for a 2nd commit!")')
 write(*,'("A new line for the 3nd commit!")')
+
+!Comment from the newly founded "new" branch
+
 end
 
 
