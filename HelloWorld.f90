18475a5b5cc0125c104339d97d424f5106c8ec5a
Luka Djigas <ldigas@gmail.com>
2011-04-20 03:54:21 +0200

    A new line on leaving branch.

--- HelloWorld.f90
+++ HelloWorld.f90
@@ -2,6 +2,7 @@ write(*,'("Hello World!")')
 write(*,'("A new line for a 2nd commit!")')
 
 write(*,'("Made a new branch: leaving - GOODBYE WORLD!")')
+write(*,'("Made a new branch: leaving - GOODBYE WORLD!")')
 
 end
 
